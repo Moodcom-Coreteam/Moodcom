5d488385d59a73cf4d3d6be22e1e781bb8b072b7
Hellois BARBOSA <hbarbosapro@gmail.com>
2021-01-10 14:06:05 +0100

    Update Flask app

--- analyse/main.py
+++ analyse/main.py
@@ -7,9 +7,40 @@ from youtube_API.youtube import getVideoCommentsThreads, getVideoCommentsTxt
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return  'Hello world from ANALYSIS part !'
+    return 'Hello world from ANALYSIS part !'
+
+
+@app.route('/analyse/', methods=['GET'])
+def analyse():
+    # Get request arguments
+    print("Get request arguments")
+    nbComments = request.args.get('nbComments')
+    videoId = request.args.get('videoId')
+
+    # Get YouTube comments
+    print("Get YouTube comments")
+    commentsThreads = getVideoCommentsThreads(int(nbComments), videoId)
+    commentsTxt = getVideoCommentsTxt(commentsThreads)
+
+    # Load the model
+    print("Get load RNN model")
+    exported_model = rnnModel()
+
+    # Doing prediction
+    print("Doing prediction")
+    exported_model.predict(commentsTxt)
+    sentimentsValue = {'joy': 0, 'sadness': 0,
+                       'fear': 0, 'love': 0, 'anger': 0}
+    for sentiments in exported_model.predict(commentsTxt):
+        sentimentsValue[getLabel(sentiments)
+                        ] = sentimentsValue[getLabel(sentiments)] + 1
+
+    print('Prediction DONE')
+    return buildAnalysisResult(videoId, sentimentsValue, 0)
+
 
 @app.errorhandler(500)
 def server_error(e):
@@ -19,31 +50,6 @@ def server_error(e):
     See logs for full stacktrace.
     """.format(e), 500
 
-@app.route('/analyse/', methods=['GET'])
-def analyse():
-    #Get request arguments
-    print("Get request arguments")
-    nbComments = request.args.get('nbComments')
-    videoId = request.args.get('videoId')
-
-    #Get YouTube comments
-    print("Get YouTube comments")
-    commentsThreads = getVideoCommentsThreads(int(nbComments), videoId)
-    commentsTxt = getVideoCommentsTxt(commentsThreads)
-
-    #Load the model
-    print("Get load RNN model")
-    exported_model = rnnModel()
-
-    #Doing prediction
-    print("Doing prediction")
-    exported_model.predict(commentsTxt)
-    sentimentsValue = {'joy': 0, 'sadness': 0, 'fear': 0, 'love': 0, 'anger': 0}
-    for sentiments in exported_model.predict(commentsTxt):
-        sentimentsValue[getLabel(sentiments)] = sentimentsValue[getLabel(sentiments)] + 1
-
-    print('Prediction DONE')
-    return buildAnalysisResult(videoId, sentimentsValue, 0)
 
 def getLabel(sentiments):
     labels = ['anger', 'fear', 'joy', 'love', 'sadness']
@@ -57,13 +63,10 @@ def getLabel(sentiments):
         cpt = cpt + 1
     return labels[index]
 
+
 def buildAnalysisResult(p_video_id, p_analysis_result, p_like_dislike_ratio):
     result = {
         '_id': str(uuid.uuid4()),
-        'title': 'no title for the moment (API in construction)',
-        'author': 'no author for the moment (API in construction)',
-        'url': 'https://www.youtube.com/watch?v=' + p_video_id,
-        'description' : 'no description for the moment (API in construction)',
         'date': str(datetime.datetime.now()),
         'analysis': {
             'feelings': p_analysis_result,
@@ -71,6 +74,7 @@ def buildAnalysisResult(p_video_id, p_analysis_result, p_like_dislike_ratio):
         }
     }
     return result
+
 
 if __name__ == "__main__":
     app.run(debug=True)
